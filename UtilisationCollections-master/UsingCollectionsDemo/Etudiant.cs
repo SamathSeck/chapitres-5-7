4534f01291bca761f59da77c8d747a3ba85c8cdc
SamathSeck <108096354+SamathSeck@users.noreply.github.com>
2024-11-22 15:57:31 +0000

    first commit

---
+++ UtilisationCollections-master/UsingCollectionsDemo/Etudiant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+
+namespace UsageCollections
+{
+    public class Etudiant
+    {
+        public string Nom { get; set; }
+        public int Age { get; set; }
+        public double NoteCC { get; set; }
+        public double NoteDevoir { get; set; }
+
+        public double CalculerMoyenne()
+        {
+            return (NoteCC * 0.33) + (NoteDevoir * 0.67);
+        }
+    }
+}
